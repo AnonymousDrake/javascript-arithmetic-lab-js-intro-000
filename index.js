ab4ef5b4bcf6eb9a42c40e9a56ec929d45e8b52b
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-08 13:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function add(x,y){
   return x+y;
 }
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(n){
+  var number=5;
+  return (number+=n);
+}
+function decrement(n){
+  var number=5;
+  return (number-=n);
+}
+function makeInt(string){
+  parseInt('string',10);
+}
+function preservaInt(string){
+  parseFloat('string',10);
+}
